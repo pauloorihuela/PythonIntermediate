bbd50f5cac58252756cb199d5b3601cfb897bb1f
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-17 23:11:28 +0100

    Create 3_MetMuseum.py
    An exercise from codedex using dictionary.

---
+++ DataStructures/3_MetMuseum.py
@@ -0,0 +1,11 @@
+# Write code below 💖
+
+archangel_gabriel = {
+  'artist': 'Gerard David',
+  'period': 'ac'
+  'date': 1510
+}
+
+print('Keys: ', archangel_gabriel.keys())
+print('Values: ', archangel_gabriel.values())
+print('Items: ', archangel_gabriel.items())
