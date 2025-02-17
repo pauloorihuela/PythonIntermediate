09ea08bb83cbcc05a697b857ea12b40c581924ad
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-17 22:44:35 +0100

    Create 2_FindMyFriends.py
    An exercise from codedex using tuples.

---
+++ DataStructures/2_FindMyFriends.py
@@ -0,0 +1,12 @@
+# Write code below 💖
+
+city = (39.466998, -0.376839)
+friend1 = (-12.046373, -77.042755)
+friend2 = (38.345997, -0.490686)
+friend3 = (43.593720, 1.439459)
+
+print("The friend who is the furthest away is ", friend1, "friend 1")
+
+tuples = friend1 + friend2 + friend3
+
+print(tuples)
