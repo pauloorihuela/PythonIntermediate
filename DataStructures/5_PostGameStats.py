d2a813fb34948ae148cda5147c19b73ba2519880
pauloorihuela <63752051+pauloorihuela@users.noreply.github.com>
2025-02-18 21:52:33 +0100

    Create 5_PostGameStats.py
    An exercie from codedex using dictionary.

---
+++ DataStructures/5_PostGameStats.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+
+player1={'name': 'Tyreek Hill', 'position': 'Wide Receiver', 'jersey_number': 10, 'yards_gained': 150, 'touchdowns': 2}
+player2={'name': 'Patrick Mahomes', 'position': 'QB', 'jersey_number': 15, 'yards_gained': 679, 'touchdowns': 5}
+player3={'name': 'Kareem Hunt', 'position': 'RB', 'jersey_number': 29, 'yards_gained': 728, 'touchdowns': 7}
+
+print('Name:', player1['name'])
+print('Position:', player1['position'])
+print('')
+print('Name:', player2['name'])
+print('Position:', player2['position'])
+print('')
+print('Name:', player3['name'])
+print('Position:', player3['position'])
+
+print(player1, player2, player3)
